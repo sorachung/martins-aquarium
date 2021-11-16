abb6f53181f468f800c8601ed975263a0c8a88bc
sorachung <sorachung.src@gmail.com>
2021-11-16 10:08:30 -0600

    added export in database.js and import in main.js

--- scripts/database.js
+++ scripts/database.js
@@ -1,7 +1,13 @@
 const database = {
-    fish: [
-        name: "Bart",
-        food: "crustaceans",
-        size: 3
-    ]
-}
+  fish: [
+    {
+      name: "Bart",
+      food: "crustaceans",
+      size: 3,
+    },
+  ],
+};
+
+export const getFish = () => {
+  return database.fish.map((fish) => ({ ...fish }));
+};
