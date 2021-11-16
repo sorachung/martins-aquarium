6103724cafada2d6403694ff75c954d5a54ad5db
sorachung <sorachung.src@gmail.com>
2021-11-16 12:50:30 -0600

    added more fish to database.js

--- scripts/database.js
+++ scripts/database.js
@@ -1,10 +1,37 @@
 const database = {
   fish: [
     {
-      name: "Bart",
-      food: "crustaceans",
-      size: 3,
+      id: 1,
+      name: "Guppers",
+      species: "Guppy",
+      size: 5,
+      location: "Alaska",
+      food: "ramen"
     },
+    {
+      id: 2,
+      name: "Goldy",
+      species: "goldfish",
+      size: 3,
+      location: "near here",
+      food: "food"
+    },
+    {
+      id: 3,
+      name: "Porg",
+      species: "Scup/Porgy",
+      size: 9,
+      location: "near mom's house",
+      food: "popcorn"
+    },
+    {
+      id:4,
+      name: "Bet",
+      species: "Siamese fighting fish/Betta",
+      size: 3,
+      location: "near here",
+      food: "food"
+    }
   ],
 };
 
